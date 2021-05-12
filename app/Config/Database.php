ab7646e8a483b969c8e8b41924b49fde4d283c0d
kinoppol <kinoppol@hotmail.com>
2021-05-13 02:20:36 +0700

    mac

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => '',
+		'password' => 'thailand',
 		'database' => 'ocs',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => 'ocs_',
