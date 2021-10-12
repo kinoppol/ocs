5486674378e9fc7156693a36cf6a0ee4b663a0d3
kinoppol <kinoppol@hotmail.com>
2021-10-12 11:44:50 +0700

    Update ReportOrg.php

--- app/Controllers/ReportOrg.php
+++ app/Controllers/ReportOrg.php
@@ -97,7 +97,7 @@ class ReportOrg extends BaseController
 					
 				$arow=array(
 					$i,
-					'school_id'=>strlim($school[$mou['school_id']],35),
+					'school_id'=>strlim($mou['school_id'],35),
 					'business_id'=>strlim($business[$mou['business_id']]['business_name'],35),
 					'job_description'=>strlim($business[$mou['business_id']]['job_description'],30),
 					'level'=>isset($mou['level'])&&$mou['level']!=''?'ระดับ '.$mou['level']:'',
