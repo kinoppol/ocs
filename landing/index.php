e496e171b1f08ae7c1d658b6680b003a6db2e3f5
kinoppol <kinoppol@hotmail.com>
2023-03-17 10:01:14 +0700

    school link

--- landing/index.php
+++ landing/index.php
@@ -53,6 +53,7 @@
 <li class="nav-item active"><a class="nav-link" href="../public/dashboard/index" href="index.html">ข้อมูลภาพรวม</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/mou/index" href="index.html">ค้นหา MOU</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/business/index" href="index.html">ค้นหาสถานประกอบการ</a></li>
+<li class="nav-item active"><a class="nav-link" href="../public/school/index" href="index.html">ข้อมูลสถานศึกษา</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/about/index" href="index.html">เกี่ยวกับระบบ</a></li>
 </ul>
 							<ul class="nav navbar-nav navbar-right">
