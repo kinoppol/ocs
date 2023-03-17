ddd1de4464ace041d69d0e4725ad13ec40f17d0d
kinoppol <kinoppol@hotmail.com>
2023-03-17 17:39:51 +0700

    dm

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -10,7 +10,7 @@ class Dashboard extends BaseController
         helper('system');
 		$data=array(
 			'title'=>'ภาพรวม',
-			'mainMenu'=>view('_menu'),
+			//'mainMenu'=>view('_menu'),
 			'content'=>view('dashboard/summary'),
 			'notification'=>'',
 			'task'=>'',
