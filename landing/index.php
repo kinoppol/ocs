80e1514e20256dc074694b048f86fba7ee76f5bb
kinoppol <kinoppol@hotmail.com>
2023-03-17 10:06:24 +0700

    inv link

--- landing/index.php
+++ landing/index.php
@@ -54,6 +54,7 @@
 <li class="nav-item active"><a class="nav-link" href="../public/mou/index" href="index.html">ค้นหา MOU</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/business/index" href="index.html">ค้นหาสถานประกอบการ</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/school/index" href="index.html">ข้อมูลสถานศึกษา</a></li>
+<li class="nav-item active"><a class="nav-link" href="../public/inv/index" href="index.html">ข้อมูลกรอ.อศ.</a></li>
 <li class="nav-item active"><a class="nav-link" href="../public/about/index" href="index.html">เกี่ยวกับระบบ</a></li>
 </ul>
 							<ul class="nav navbar-nav navbar-right">
