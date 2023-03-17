385116f48b7ab685026c17021c3f93e7ae0cf39e
kinoppol <kinoppol@hotmail.com>
2023-03-17 17:48:58 +0700

    dp

--- app/Views/dashboard/mou.php
+++ app/Views/dashboard/mou.php
@@ -1,3 +1,7 @@
+<div class="container-fluid">
+            <div class="block-header">
+                <h2 style="text-align:left;float:left;"><?php print $title;?></h2><h2 style="text-align:right;float:right;"><?php /* print $_SERVER['PATH_INFO']; */ ?></h2>
+                <hr style="clear:both;"/>
 <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
@@ -31,5 +35,7 @@
                             </div>
                     </div>
                             </div>
+                            </div>
+                    </div>
                             </div>
                     </div>
