2c39062a86d360cb42aed6fcdb75be587e307511
kinoppol <kinoppol@hotmail.com>
2023-03-18 08:50:42 +0700

    mm

--- app/Views/dashboard/mou.php
+++ app/Views/dashboard/mou.php
@@ -1,13 +1,13 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-			<form role="form">
+			<form role="form" action="<?php print site_url('public/dashboard/mou'); ?>" method="post">
 				<div class="form-group">
 					 
 					<label for="exampleInputEmail1">
 						ค้นหา MOU
 					</label>
-					<input type="email" class="form-control" id="exampleInputEmail1" />
+					<input type="text" class="form-control" id="q" name="q" />
 				</div>
 				<button type="submit" class="btn btn-primary">
 					ค้นหา
