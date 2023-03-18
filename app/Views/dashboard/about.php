657fb0804edff619930292278f02f51246279b36
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:01:46 +0700

    ea

--- app/Views/dashboard/about.php
+++ app/Views/dashboard/about.php
@@ -7,8 +7,8 @@
                     สำนักงานคณะกรรมการการอาชีวศึกษา</a><br />
                      &copy; 2021 - <?php print date('Y'); ?><br />
                      อำนวยการพัฒนาโดย<br />
-                     2021 - วิทยาลัยสารพัดช่างสมุทรปราการ <a href="http://spkpoly.ac.th">http://spkpoly.ac.th</a><br />
-                     2023 - วิทยาลัยอาชีวศึกษาร้อยเอ็ด <a href="https://www.rvc.ac.th">https://www.rvc.ac.th</a><br />
+                     2021 - วิทยาลัยสารพัดช่างสมุทรปราการ <a href="http://spkpoly.ac.th" target="_blank">http://spkpoly.ac.th</a><br />
+                     2023 - วิทยาลัยอาชีวศึกษาร้อยเอ็ด <a href="http://www.rvc.ac.th" target="_blank">http://www.rvc.ac.th</a><br />
                       สอบถามปัญหาการใช้งาน <a href="tel:+660919968266"> 09-1996-8266</a>
 			</address>
 		</div>
