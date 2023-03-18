ffaf947ae32b1becfa6aef710f1faa62e930aef2
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:00:36 +0700

    ea

--- app/Views/dashboard/about.php
+++ app/Views/dashboard/about.php
@@ -3,7 +3,13 @@
 		<div class="col-md-12">
 			 
 			<address>
-				 <h3>ระบบฐานข้อมูลสำนักความร่วมมือ</h3><a href="https://www.vec.go.th" target="_blank">สำนักงานคณะกรรมการการอาชีวศึกษา</a><br /> &copy; 2021 - <?php print date('Y'); ?><br /> สอบถามปัญหาการใช้งาน <a href="tel:+660919968266"> 09-1996-8266</a>
+				 <h3>ระบบฐานข้อมูลสำนักความร่วมมือ</h3><a href="https://www.vec.go.th" target="_blank">
+                    สำนักงานคณะกรรมการการอาชีวศึกษา</a><br />
+                     &copy; 2021 - <?php print date('Y'); ?><br />
+                     อำนวยการพัฒนาโดย<br />
+                     2021 - วิทยาลัยสารพัดช่างสมุทรปราการ <a href="http://spkpoly.ac.th">http://spkpoly.ac.th</a><br />
+                     2023 - วิทยาลัยอาชีวศึกษาร้อยเอ็ด <a href="https://www.rvc.ac.th">https://www.rvc.ac.th</a><br />
+                      สอบถามปัญหาการใช้งาน <a href="tel:+660919968266"> 09-1996-8266</a>
 			</address>
 		</div>
 	</div>
