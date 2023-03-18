0496723681704cdf012cf902e75575a3372abdd6
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:53:40 +0700

    dd

--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -9,12 +9,11 @@
 	<div class="row">
 	<div class="col-md-4">
 		  <div class="card">
-		  <div class="col-md-3 col-sm-6 col-xs-12">
-<div class="info-box">
-<span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">Messages</span>
-<span class="info-box-number">1,410</span>
+				<div class="info-box">
+				<span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+				<div class="info-box-content">
+				<span class="info-box-text">Messages</span>
+				<span class="info-box-number">1,410</span>
 		  </div>
 		  </div>
 		<div class="col-md-4">
