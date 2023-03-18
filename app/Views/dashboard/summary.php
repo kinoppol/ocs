24564a721de30f71c6ac056a76f90c1ca460578a
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:52:44 +0700

    ds

--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -9,19 +9,12 @@
 	<div class="row">
 	<div class="col-md-4">
 		  <div class="card">
-		    <div class="info-box bg-primary">
-		      <span class="info-box-icon">
-		        <i class="fa fa-bookmark-o"></i>
-		      </span>
-		      <div class="info-box-content">
-		        <span class="info-box-text">Bookmarks</span>
-		        <span class="info-box-number">41,410</span>
-		        <div class="progress">
-		          <div class="progress-bar" style="width: 70%"></div>
-		        </div>
-		        <span class="progress-description"> 70% Increase in 30 Days </span>
-		      </div>
-		    </div>
+		  <div class="col-md-3 col-sm-6 col-xs-12">
+<div class="info-box">
+<span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+<div class="info-box-content">
+<span class="info-box-text">Messages</span>
+<span class="info-box-number">1,410</span>
 		  </div>
 		  </div>
 		<div class="col-md-4">
