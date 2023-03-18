f5b9a07fec9518dbbe9da1d8a74ab3decd7b460c
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:55:18 +0700

    dd

--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -10,7 +10,7 @@
 	<div class="col-md-4">
 		  <div class="card">
 				<div class="info-box">
-				<span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+				<span class="info-box-icon bg-primary"><i class="fa fa-envelope-o"></i></span>
 				<div class="info-box-content">
 				<span class="info-box-text">Messages</span>
 				<span class="info-box-number">1,410</span>
