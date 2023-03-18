73b5158222dac0b898b068e55615edb316e993ed
kinoppol <kinoppol@hotmail.com>
2023-03-18 10:50:32 +0700

    dd

--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -7,26 +7,23 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4">
-
-		<div class="card">
-		<div class="info-box bg-aqua">
-<span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">Bookmarks</span>
-<span class="info-box-number">41,410</span>
-<div class="progress">
-<div class="progress-bar" style="width: 70%"></div>
-</div>
-<span class="progress-description">
-70% Increase in 30 Days
-</span>
-</div>
-</div>
-
-</div>
-
-		</div>
+	<div class="col-md-4">
+		  <div class="card">
+		    <div class="info-box bg-aqua">
+		      <span class="info-box-icon">
+		        <i class="fa fa-bookmark-o"></i>
+		      </span>
+		      <div class="info-box-content">
+		        <span class="info-box-text">Bookmarks</span>
+		        <span class="info-box-number">41,410</span>
+		        <div class="progress">
+		          <div class="progress-bar" style="width: 70%"></div>
+		        </div>
+		        <span class="progress-description"> 70% Increase in 30 Days </span>
+		      </div>
+		    </div>
+		  </div>
+		  </div>
 		<div class="col-md-4">
 			<div class="card">
 				<h5 class="card-header">
