215e638f76576fbf0e02ce93f9c5883810ba409f
kinoppol <kinoppol@hotmail.com>
2023-03-18 08:45:38 +0700

    mm

--- app/Views/dashboard/mou.php
+++ app/Views/dashboard/mou.php
@@ -1,31 +1,50 @@
-                    <div class="card">
-                    <div class="header">
-                               <form action="<?php print site_url('public/'.$_SERVER['PATH_INFO']); ?>" method="post">
-                                    </div><div class="row clearfix">
-                                      <div class="col-lg-8 col-md-12 col-sm-12 col-xs-6">
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" name="q" placeholder="คำค้น.." value="<?php print isset($_POST['q'])?$_POST['q']:''; ?>">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-2 col-sm-2 col-xs-3">
-                                        <div class="form-group">
-                                                <button class="btn btn-primary"><i class="material-icons">search</i> แสดง</button>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-2 col-md-3 col-sm-3 col-xs-3">
-                                        <div class="form-group">
-                                                <!-- <button  type="button" class="btn btn-warning" id="addBusiness"><i class="material-icons">add</i> เพิ่มข้อมูลสถานประกอบการ</button> -->
-                                        </div>
-                                    </div>
-                                </div>
-                               </form>
-                        <div class="body">
-                        <div class="table-responsive">
-                        <?php
-                                //print genTable($businessArr);
-                             ?>
-                            </div>
-                    </div>
-                            </div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<form role="form">
+				<div class="form-group">
+					 
+					<label for="exampleInputEmail1">
+						Email address
+					</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" />
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputPassword1">
+						Password
+					</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" />
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputFile">
+						File input
+					</label>
+					<input type="file" class="form-control-file" id="exampleInputFile" />
+					<p class="help-block">
+						Example block-level help text here.
+					</p>
+				</div>
+				<div class="checkbox">
+					 
+					<label>
+						<input type="checkbox" /> Check me out
+					</label>
+				</div> 
+				<button type="submit" class="btn btn-primary">
+					Submit
+				</button>
+			</form>
+			<h2>
+				Heading
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+	</div>
+</div>
