4e861bf308f3b32c264694b1791489a575ab3742
kinoppol <kinoppol@hotmail.com>
2023-03-20 14:15:36 +0700

    mtt

--- app/Views/dashboard/mouTable.php
+++ app/Views/dashboard/mouTable.php
@@ -1,3 +1,7 @@
+<?php 
+
+helper('thai');
+?>
 <table class="table">
 				<thead>
 					<tr>
--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -1,7 +1,3 @@
-<?php 
-
-helper('thai');
-?>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
