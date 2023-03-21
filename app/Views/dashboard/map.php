f99c7bf9063f6b2e397b8658300a8ef4f06ca460
kinoppol <kinoppol@hotmail.com>
2023-03-21 15:54:22 +0700

    em

--- app/Views/dashboard/map.php
+++ app/Views/dashboard/map.php
@@ -10,7 +10,7 @@
   <body>
     <div id="map"></div>
     <style type="text/css">
-.legend{color: black; width: 300px; font-family: arial; font-size: 14px;}    
+.legend{color: black; width: 100%px; font-family: arial; font-size: 14px;}    
 .legend_color {display: table; width: 100%; background: white; list-style: none; margin: 0px; padding: 0px; }
 .legend_color li{width: 20%;  height: 20px; display: table-cell;}
 .legend_label {display: table; width: 100%;  padding: 0px; padding-left: 10%; padding-right: 10%; list-style: none; margin: 0px; box-sizing: border-box;}
