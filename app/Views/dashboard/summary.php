0924f80339e58e08be44040383c30a8280dc3b5f
kinoppol <kinoppol@hotmail.com>
2023-03-21 16:16:12 +0700

    ra

--- app/Views/dashboard/summary.php
+++ app/Views/dashboard/summary.php
@@ -88,7 +88,7 @@
 				</h5>
 				<div class="card-body">
 					<p class="card-text">
-					<div class="embed-responsive embed-responsive-3by4">
+					<div class="embed-responsive embed-responsive-1by1">
   						<iframe class="embed-responsive-item" src="<?php print site_url('public/dashboard/map?disp=embed'); ?>"></iframe>
 					</div>
 					</p>
