69af93d89ee6501478fd83efb1245157482f3256
kinoppol <kinoppol@hotmail.com>
2023-03-23 13:26:10 +0700

    Update ProvinceModel.php

--- app/Models/ProvinceModel.php
+++ app/Models/ProvinceModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 
 class SummaryModel extends Model
 {
-    public function getProvince($province_id){
+    public function getProvince($province_id=false){
         $db = \Config\Database::connect();
         $builder = $db->table('data_province');
         $data=$builder->get()->getResult();
